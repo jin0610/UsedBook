b46b42c40cba7ca7d2682798e46749c6b1401671
jin0610 <sji302777@gmail.com>
2022-06-11 13:02:01 +0900

    컴포넌트 요소 삽입

--- src/components/HomePage/HomeForm.js
+++ src/components/HomePage/HomeForm.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom";
+import "./HomeForm.css"
 
 const HomeForm = () =>{
+
+    const [visible, setVisible] = useState(false);
+
     return(
-        <div>
-            
+        <div class="homeform">
             <Link to="/mypage">mypage</Link>
+            <div class="searchboxblock">
+                <input type="text"  class="searchbox"/>
+                <button class="material-symbols-outlined">search</button>
+            </div>
+            <div>
+                <button onClick={()=>{setVisible(!visible)}}>
+                    {visible? "학과 숨기기" :"학과 보이기"}
+                </button>
+                {visible && 
+                    <div>
+                        학과
+                    </div>
+                }
+
+
+            </div>
+            
         </div>
     )
 }
 
 export default HomeForm;
+
+
+// https://fonts.google.com/icons?preview.size=37
