a21fe9e121f1f4feedab4a10f9c0618815ab1d8d
jin0610 <sji302777@gmail.com>
2022-06-12 04:31:59 +0900

    토글 추가

--- src/components/HomePage/HomeForm.js
+++ src/components/HomePage/HomeForm.js
@@ -5,7 +5,11 @@ import "./HomeForm.css"
 const HomeForm = () =>{
 
     const [visible, setVisible] = useState(false);
-
+    const [isOpen, setMenu] = useState(false);
+    const college = ["본부직속", "생명산업과학대학", "사회과학대학", "인문예술대학","공과대학","사범대학","약학대학","미래융합대학", "융합전공"]
+    const toggleMenu = () =>{
+        setMenu(isOpen => !isOpen);
+    }
     return(
         <div class="homeform">
             <Link to="/mypage">mypage</Link>
@@ -18,8 +22,16 @@ const HomeForm = () =>{
                     {visible? "학과 숨기기" :"학과 보이기"}
                 </button>
                 {visible && 
-                    <div>
-                        학과
+                    <div>  
+                        <div>
+                            {college.map((college) =>(<button onClick={()=>toggleMenu()}>{college}</button>))}
+                        </div>
+                        {isOpen &&
+                            <ul>
+                                <li>1</li>
+                                <li>2</li>
+                            </ul>
+                        }
                     </div>
                 }
 
