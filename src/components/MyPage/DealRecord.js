8655474e02a2f28bf6fcf081c735c1b7ef9de539
jin0610 <sji302777@gmail.com>
2022-08-07 23:27:06 +0900

    component 추가

---
+++ src/components/MyPage/DealRecord.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const DealRecord = () =>{
+    return(
+        <div>
+            <div>거래 기록</div>
+            <div>
+                <img className="BorrowBookImg" src="https://newsimg.sedaily.com/2022/05/12/265XXK6D83_1.jpg"></img>
+                <p>이름</p>
+                <p>구매날짜</p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default DealRecord;
--- src/components/MyPage/MyPageFrom.js
+++ src/components/MyPage/MyPageFrom.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BorrowRecord from "./BorrowRecord";
+import ChangePwd from "./ChangePwd";
+import DealRecord from "./DealRecord";
 
 
 const MyPageForm = () =>{
+
+    
+
     return(
         <div>
-            <Link to={"/"}>homepage</Link>
+            <Link to={"/home"}>homepage</Link>
+            <div>
+                <button>거래 기록</button>
+                <button>대출 기록</button>
+                <button>비밀번호 변경</button>
+            </div>
+            <DealRecord/>
+            <BorrowRecord/>
+            <ChangePwd/>
         </div>
     )
 }
