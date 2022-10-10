de8784327beb2b7b52c9a7282ec0b24fa8aca804
jin0610 <sji302777@gmail.com>
2022-10-11 02:09:22 +0900

    회원가입 추가

--- src/Components/AuthPage/SignupForm.js
+++ src/Components/AuthPage/SignupForm.js
@@ -13,24 +13,24 @@ const SignupForm = (props) =>{
                     <div class="row justify-content-center mb-4 mt-5">
                         <div class="col-7">
                             <div class="form-group">
-                                <input class="form-control" id="std_num" type="text" placeholder="STUDENT NUMBER" name="std_num" />
+                                <input class="form-control" id="std_num" type="text" placeholder="STUDENT NUMBER" name="std_num" onChange={onChange} value={form.std_num}/>
                                 <div class="invalid-feedback">A student number is required.</div>
                             </div>
                             <div class="form-group">
-                                <input class="form-control" id="password" type="password" placeholder="PASSWORD"/>
+                                <input class="form-control" id="password" type="password" placeholder="PASSWORD" onChange={onChange} value={form.password}/>
                                 <div class="invalid-feedback">A password is required.</div>
                             </div>
                             <div class="form-group">
-                                <input class="form-control" id="password CHECK" type="password" placeholder="PASSWORD CHECK"/>
+                                <input class="form-control" id="password2" type="password" placeholder="PASSWORD CHECK" onChange={onChange} value={form.password2}/>
                                 <div class="invalid-feedback">A password is required.</div>
                                 <div class="invalid-feedback">Password is not correct.</div>
                             </div>
                             <div class="form-group">
-                                <input class="form-control" id="email" type="email" placeholder="EMAIL"/>
+                                <input class="form-control" id="email" type="email" placeholder="EMAIL" onChange={onChange} value={form.email}/>
                                 <div class="invalid-feedback">An email is required.</div>
                             </div>
                             <div class="form-group">
-                                <input class="form-control" id="phonenum" type="text" placeholder="PHONE NUMBER"/>
+                                <input class="form-control" id="phonenum" type="text" placeholder="PHONE NUMBER" onChange={onChange} value={form.phonenum}/>
                                 <div class="invalid-feedback">A phone number is required.</div>
                             </div>
 						</div>
--- src/Containers/SignupContainer.js
+++ src/Containers/SignupContainer.js
@@ -4,11 +4,68 @@ const SignupContainer = () =>{
     const [form, setForm] = useState({
         std_name:"",
         password:"",
+        password2:"",
         email: "",
         phonenumber:""
     })
+
+    const onChange = e => {
+        const { value, name } = e.target;
+        setForm({
+            ...form,
+            [name] : value
+        })
+    };
+
+    //폼 등록 이벤트 핸들러
+    const onSubmit =e =>{
+        console.log("회원가입")
+        e.preventDefault();
+        const { std_name,  password, password2, email, phonenumber} = form;
+        // 하나라도 비어있다면
+        if([std_name,  password, password2, email, phonenumber].includes('')){
+            alert('빈칸을 모두 입력하세요.');
+            return;
+        }
+        if (password !== password2){
+            alert('비밀번호가 일치하지 않습니다.');
+            setForm({
+                ...form,
+                password: '',
+                password2: '',
+            })
+            return;
+        }
+        const data = {
+            std_name,  password, email, phonenumber
+        };
+        // client.post('/api/auth/signup',queryString.stringify(data)).then(res => {
+        //     console.log(res)
+        //     console.log(data)
+    
+        //     if(res.status=== 200){
+        //         console.log('회원가입 성공');
+        //         alert('회원가입 성공');
+        //         document.location.href = '/'
+        //     }
+            
+        // }) 
+    };
+
+    // 컴포넌트가 처음 렌더링될때 form을 초기화함
+    useEffect(() => {
+        setForm({
+            std_name:"",
+            password:"",
+            password2:"",
+            email: "",
+            phonenumber:""
+        })
+    }, []);
     return(
-        <SignupForm form={form}/>
+        <SignupForm form={form}
+        onChange={onChange}
+        onSubmit={onSubmit}/>
     )
 }
 
